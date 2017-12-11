243ca74fa79b84fe91db7ee3aa7232a5037aa5f0
David McGowan <david.mcgowan@screenmedia.co.uk>
2017-12-11 21:27:15 +0000

    Add project files.

---
+++ ConsoleApp1/Entities/CustomerUS.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace ConsoleApp1.Entities
+{
+    class CustomerUS : TableEntity
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public CustomerUS(string name, string email)
+        {
+            this.Name = name;
+            this.Email = email;
+            this.PartitionKey = "US";
+            this.RowKey = email;
+        }
+
+        public CustomerUS()
+        {
+            
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp1.Entities;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("StorageConnection"));
+
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+
+            CloudTable table = tableClient.GetTableReference("customers");
+
+            table.CreateIfNotExists();
+
+            //CreateCustomer(table, new CustomerUS("David", "david@test.com"));
+            GetCustomer(table, "US", "david@test.com");
+
+            Console.ReadKey();
+        }
+
+        static void CreateCustomer(CloudTable table, CustomerUS customer)
+        {
+            TableOperation insert = TableOperation.Insert(customer);
+
+            table.Execute(insert);
+        }
+
+        static void GetCustomer(CloudTable table, string partitionKey, string rowKey)
+        {
+            TableOperation retrieve = TableOperation.Retrieve<CustomerUS>(partitionKey, rowKey);
+
+            var result = table.Execute(retrieve);
+
+            Console.WriteLine(((CustomerUS) result.Result).Name);
+        }
+    }
+}
